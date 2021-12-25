1ef8b0eb6fe1066693634cfcc5a435340b59b04b
Adel Valiullin <happosai10@gmail.com>
2021-12-25 19:14:52 +0300

    update StatisticsServiceTest

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxNext() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
